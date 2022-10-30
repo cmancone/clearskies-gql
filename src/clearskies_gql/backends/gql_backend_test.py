98452ab37e41e6d1f465d3fcdb0c1f3543c5d53b
Conor Mancone <cmancone@gmail.com>
2022-10-30 08:16:38 -0400

    Basics

--- src/clearskies_gql/backends/gql_backend_test.py
+++ src/clearskies_gql/backends/gql_backend_test.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 from types import SimpleNamespace
 from .gql_backend import GqlBackend
 import clearskies
-from ..di import StandardDependencies
+from clearskies.di import StandardDependencies
 from boto3.dynamodb import conditions as dynamodb_conditions
 class User(clearskies.Model):
     def __init__(self, gql_backend, columns):
@@ -25,3 +25,16 @@ class Users(clearskies.Models):
 class GqlBackendTest(unittest.TestCase):
     def setUp(self):
         self.di = StandardDependencies()
+
+    def test_configure(self):
+        environment = SimpleNamespace(get=MagicMock(return_value='https://env.example.com'))
+        backend = GqlBackend('requests', environment)
+        backend.configure(url='https://example.com', auth='auth')
+        self.assertEquals('https://example.com', backend.url)
+        self.assertEquals('auth', backend._auth)
+        environment.get.assert_not_called()
+
+        backend.configure()
+        self.assertEquals('https://env.example.com', backend.url)
+        self.assertEquals(None, backend._auth)
+        environment.get.assert_called_with('gql_server_url', silent=True)
